6aee856511bf28a5c1330395112690c21419185e
JonnyC <conneryjonathan@gmail.com>
2024-10-27 23:22:05 +0000

    add build steps for backend

---
+++ backend/build.js
@@ -0,0 +1,10 @@
+const esbuild = require('esbuild')
+
+esbuild.build({
+    entryPoints: ['./dist/index.js'],
+    bundle: true,
+    platform: 'node',
+    target: 'node18',
+    outfile: './build/bundle.js',
+    minify: true
+}).catch(() => process.exit(1))
