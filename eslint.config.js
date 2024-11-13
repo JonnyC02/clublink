e384f6f50e227a197e7315eef519d5bfe8c502c8
JonnyC <conneryjonathan@gmail.com>
2024-11-13 13:52:09 +0000

    update eslint config

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import { ESLint } from 'eslint';
+
+/** @type {ESLint.ConfigData} */
+export default {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-console': 'warn',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
