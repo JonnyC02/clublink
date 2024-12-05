12a45acb74df7647a3102584f6e424350d2dc124
JonnyC <conneryjonathan@gmail.com>
2024-12-05 14:35:11 +0000

    fix snyk vulns

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import {hidePoweredBy} from 'helmet'
+import csurf from 'csurf';
 
 const app: Express = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors())
+app.use(hidePoweredBy());
+app.use(csurf());
 
 app.get('/health', (req: Request, res: Response) => {
   res.status(200).json({ message: "Health Check!" });
