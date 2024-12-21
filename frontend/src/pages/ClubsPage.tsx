2f49fb39efeb7de9e837fff39b24431dd5b46125
Jonny C <conneryjonathan@gmail.com>
2024-12-21 13:34:26 +0000

    small fixes

--- frontend/src/pages/ClubsPage.tsx
+++ frontend/src/pages/ClubsPage.tsx
@@ -3,16 +3,16 @@ import TitleSection from "../components/TitleSection";
 import React from "react";
 
 const links = [
-    { label: 'Home', href: '/'},
-    { label: 'Browse Clubs', href: '/clubs'},
-    { label: 'Events', href: '#'},
-    { label: 'About', href: '#'}
+    { label: 'Home', href: '/' },
+    { label: 'Browse Clubs', href: '/clubs' },
+    { label: 'Events', href: '#' },
+    { label: 'About', href: '#' }
 ]
 
 const cta = (
     <>
-        <a href='/clubs' className='px-8 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600'>Explore Clubs</a>
-        <a href="/login" className="px-8 py-3 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200">Get Started</a>
+        <a href="/clubs" className='text-sm text-gray-600 hover:text-gray-900'>Explore Clubs</a>
+        <a href="/login" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Get Started</a>
     </>
 )
 
