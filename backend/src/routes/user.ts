094be3a1ffba13db338983d3ef3e4300344ad399
Jonny C <conneryjonathan@gmail.com>
2025-01-06 15:24:52 +0000

    fix eslint error

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -5,7 +5,7 @@ import { getAllClubs } from '../utils/user';
 const router = express.Router();
 
 router.get('/clubs', authenticateToken, async (req: Request, res: Response) => {
-    const clubs = await getAllClubs((req as any).user?.id)
+    const clubs = await getAllClubs((req as any).user?.id) // eslint-disable-line @typescript-eslint/no-explicit-any
     res.json(clubs)
 })
 
