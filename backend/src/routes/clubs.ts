c6aec8af9b088dc7956a9ee2318d36348586ed9a
JonnyC <conneryjonathan@gmail.com>
2025-01-12 01:19:02 +0000

    fix

--- backend/src/routes/clubs.ts
+++ backend/src/routes/clubs.ts
@@ -17,7 +17,7 @@ const s3 = new S3Client({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
     },
-    region: process.env.AWS_REGION || '',
+    region: process.env.AWS_REGION || 'us-east-1',
 });
 
 const upload = multer({
