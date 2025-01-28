2e38af1b87707d05b69071c75802f647d6983e5b
JonnyC <conneryjonathan@gmail.com>
2025-01-28 15:24:38 +0000

    fix ci

--- backend/tests/integration/clubs.test.ts
+++ backend/tests/integration/clubs.test.ts
@@ -7,6 +7,15 @@ import stripe from "../../src/utils/stripe";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const mockStripe = stripe as jest.Mocked<typeof stripe>;
 
+jest.mock("../../src/utils/stripe", () => ({
+  paymentIntents: {
+    create: jest.fn(),
+  },
+  webhooks: {
+    constructEvent: jest.fn(),
+  },
+}));
+
 jest.mock("../../src/db/db", () => ({
   query: jest.fn(),
 }));
