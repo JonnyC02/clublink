c71613c5b622592901152963715488eebcfccc72
JonnyC <conneryjonathan@gmail.com>
2025-02-07 13:30:42 +0000

    cleanup audit logic (#33)

--- backend/src/utils/audit.ts
+++ backend/src/utils/audit.ts
@@ -3,11 +3,11 @@ import pool from "../db/db";
 export const addAudit = async (
   clubId: number,
   userId: number | undefined,
-  memberId: number | undefined,
+  actorId: number | undefined,
   action: string
 ) => {
   await pool.query(
     `INSERT INTO auditlog (clubid, memberid, userid, actiontype) VALUES ($1, $2, $3, $4);`,
-    [clubId, memberId, userId, action]
+    [clubId, actorId, userId, action]
   );
 };
