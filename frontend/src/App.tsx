4a559751d4b8c00a9a7c5b3136e913cbc1f661b0
JonnyC <conneryjonathan@gmail.com>
2025-03-12 17:09:49 +0000

    boost lighthouse scores

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,19 @@ import ErrorPage from "./pages/ErrorPage";
 import ClubsPage from "./pages/ClubsPage";
 import AuthPage from "./pages/AuthPage";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Dashboard from "./pages/Dashboard";
-import VerifyPage from "./pages/VerifyPage";
-import ClubPage from "./pages/ClubPage";
-import ClubDashboard from "./pages/ClubDashboard";
 import CommitteeProtected from "./components/CommitteeProtected";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
-import AboutPage from "./pages/AboutPage";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import Checkout from "./pages/Checkout";
+import AboutPage from "./pages/AboutPage";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import VerifyPage from "./pages/VerifyPage";
+import ClubPage from "./pages/ClubPage";
 import Action from "./pages/Action";
 import StudentVerify from "./pages/StudentVerify";
+import Dashboard from "./pages/Dashboard";
+import ClubDashboard from "./pages/ClubDashboard";
+import Checkout from "./pages/Checkout";
 
 const stripePromise = process.env.REACT_APP_PUBLISH_KEY
   ? loadStripe(process.env.REACT_APP_PUBLISH_KEY)
@@ -47,52 +47,52 @@ function App() {
   }, []);
   return (
     <Router>
-      <Elements stripe={stripePromise}>
-        <Routes>
-          {backendOnline && stripePromise ? (
-            <>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/clubs" element={<ClubsPage />} />
-              <Route path="/login" element={<AuthPage isSignup={false} />} />
-              <Route path="/signup" element={<AuthPage isSignup={true} />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/reset-password" element={<ResetPassword />} />
-              <Route path="/verify" element={<VerifyPage />} />
-              <Route path="/club/:id" element={<ClubPage />} />
-              <Route path="/accept" element={<Action />} />
-              <Route path="/deny" element={<Action />} />
-              <Route path="/studentVerify" element={<StudentVerify />} />
-              <Route
-                path="/dashboard"
-                element={
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/club/:id/committee"
-                element={
-                  <CommitteeProtected>
-                    <ClubDashboard />
-                  </CommitteeProtected>
-                }
-              />
-              <Route
-                path="/payment/:id"
-                element={
-                  <ProtectedRoute>
+      <Routes>
+        {backendOnline && stripePromise ? (
+          <>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/clubs" element={<ClubsPage />} />
+            <Route path="/login" element={<AuthPage isSignup={false} />} />
+            <Route path="/signup" element={<AuthPage isSignup={true} />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/verify" element={<VerifyPage />} />
+            <Route path="/club/:id" element={<ClubPage />} />
+            <Route path="/accept" element={<Action />} />
+            <Route path="/deny" element={<Action />} />
+            <Route path="/studentVerify" element={<StudentVerify />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/club/:id/committee"
+              element={
+                <CommitteeProtected>
+                  <ClubDashboard />
+                </CommitteeProtected>
+              }
+            />
+            <Route
+              path="/payment/:id"
+              element={
+                <ProtectedRoute>
+                  <Elements stripe={stripePromise}>
                     <Checkout />
-                  </ProtectedRoute>
-                }
-              />
-            </>
-          ) : (
-            <Route path="*" element={<ErrorPage />} />
-          )}
-        </Routes>
-      </Elements>
+                  </Elements>
+                </ProtectedRoute>
+              }
+            />
+          </>
+        ) : (
+          <Route path="*" element={<ErrorPage />} />
+        )}
+      </Routes>
     </Router>
   );
 }
