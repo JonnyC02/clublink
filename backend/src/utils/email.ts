eb4bc5df385ab689d3c572a75c31baf098ea7185
JonnyC <conneryjonathan@gmail.com>
2025-03-31 17:48:13 +0100

    small email fix

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -45,10 +45,15 @@ export const sendEmail = async (email: string, mailOptions: MailOptions) => {
   }
 
   const transport = nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 
