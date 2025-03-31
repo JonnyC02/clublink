13b7b1c9f5f1d958dc5a4adc6295f8074d7876e5
JonnyC <conneryjonathan@gmail.com>
2025-03-31 01:07:37 +0100

    Release (#81)
    * Deploy rebase (#73)
    
    deployment rebase
    
    * Deploy (#75)
    
    * deploy script (#71) (#72)
    
    * deployment script
    
    * gitignore pm2
    
    * update deploy.yml
    
    * change env var
    
    * change back env vars
    
    * Deploy rebase (#73) (#74)
    
    deployment rebase
    
    * small fix
    
    * metadata change (#78)
    
    * mini bugs (#80)

--- frontend/tests/unit/Footer.test.tsx
+++ frontend/tests/unit/Footer.test.tsx
@@ -1,19 +1,23 @@
-import Footer from '../../src/components/Footer';
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import Footer from "../../src/components/Footer";
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-describe('Footer Component', () => {
-    it('Renders footer correctly', () => {
-        render(<Footer />)
+describe("Footer Component", () => {
+  it("Renders footer correctly", () => {
+    render(<Footer />);
 
-        expect(screen.getByText('ClubLink')).toBeDefined();
-        expect(screen.getByText('Discover and manage your clubs in one place')).toBeDefined();
-        expect(screen.getByText('Quick Links')).toBeDefined();
-        expect(screen.getByText('About')).toBeDefined();
-        expect(screen.getByText('Contact')).toBeDefined();
-        expect(screen.getByText('Privacy Policy')).toBeDefined();
-        expect(screen.getByText('Stay Updated')).toBeDefined();
-        expect(screen.getByText('Subscribe')).toBeDefined();
-        expect(screen.getByText('© 2024 Clublink. All rights reserved')).toBeDefined();
-    })
-})
+    expect(screen.getByText("ClubLink")).toBeDefined();
+    expect(
+      screen.getByText("Discover and manage your clubs in one place")
+    ).toBeDefined();
+    expect(screen.getByText("Quick Links")).toBeDefined();
+    expect(screen.getByText("About")).toBeDefined();
+    expect(screen.getByText("Contact")).toBeDefined();
+    expect(screen.getByText("Privacy Policy")).toBeDefined();
+    expect(screen.getByText("Stay Updated")).toBeDefined();
+    expect(screen.getByText("Subscribe")).toBeDefined();
+    expect(
+      screen.getByText("© 2025 ClubLink. All rights reserved")
+    ).toBeDefined();
+  });
+});
